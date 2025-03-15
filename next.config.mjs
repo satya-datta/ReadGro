d3ec3111f17dde7e9b5c69d3862a28d4620c908c
Satya <satyadattakallepalli@gmail.com>
2025-03-15 18:15:28 +0530

    before hosting into cloud after a error

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,13 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ["localhost"],
+    domains: ["localhost", "readgro-backend.onrender.com"],
   },
   serverRuntimeConfig: {
-    port: process.env.PORT || 3000,
-    host: "0.0.0.0",
+    port: process.env.PORT || 10000,
+    host: "0.0.0.0", // Required for Render
   },
+  output: "standalone",
 };
 
 export default nextConfig;
